289b23c5562e54bf431b94e790c70165ae49e83d
jyjokokk <jyrki.j.kokkola@student.jyu.fi>
2020-05-21 11:30:27 +0300

    Added all the core functions.

--- pydictnotes.py
+++ pydictnotes.py
@@ -12,17 +12,74 @@ import json
 NOTES = {}
 
 
-def add_entry(entry: str, *args: str) -> bool:
+def add_entry(entry: str, description: str, *args: str) -> bool:
     """Adds a new entry, with optional tags
 
     Args:
         entry (str): Name (and key) of the new entry.
-    
+        description(str): Description for the entry.
+
     Returns:
         bool -- If operation succeeded
     """
-    if len(args) < 1:
-        NOTES[entry] = {}
-        return True
-    NOTES[entry] = set(args)
+    tags = set()
+    for a in args:
+        tags.add(a.lower)
+    NOTES[entry.lower] = {
+        "description": description,
+        "tags": tags
+    }
     return True
+
+
+def edit_description(entry: str, new_desc: str):
+    """Updates the description of an entry 
+
+    Args:
+        entry (str): Entry to edit
+        new_desc (str): New description for the entry
+    """
+    NOTES[entry.lower]['desc'] = new_desc
+
+
+def add_tag(entry: str, tag: str) -> bool:
+    """Add a new tag to an entry.
+
+    Args:
+        entry (str): Entry which tags to update
+        tag (str): New tag
+
+    Returns:
+        bool: if successfull
+    """
+    NOTES[entry.lower]['tags'].add(tag.lower)
+    return True
+
+
+def remove_tag(entry: str, tag: str):
+    """Removes the given tag from the specified entry.
+
+    Args:
+        entry (str): Entry to edit
+        tag (str): Tag to remove
+    """
+    NOTES[entry.lower]['tags'].remove(tag.lower)
+
+
+def update_tags(entry: str, new_tags: set = set()):
+    """Removes all tags from the specified entry.
+
+    Args:
+        entry (str): Entry to edit
+        new_tags (set): New tags
+    """
+    NOTES[entry.lower]['tags'] = new_tags
+
+
+def delete_entry(entry: str):
+    """Deletes the specified entry.
+
+    Args:
+        entry (str): Entry to delete.
+    """
+    NOTES.pop(entry.lower)
